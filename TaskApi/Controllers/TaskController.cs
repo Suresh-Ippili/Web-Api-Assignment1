8f3390f37d96047b844e1b099b4b4c50798165ff
vysyaraju rakesh kumar <rakeshkumarvysyaraju@gmail.com>
2024-09-01 12:57:11 +0530

    Web-Api-Assignment1 is fixed

---
+++ TaskApi/Controllers/TaskController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskApi.DataAccess;
+using TaskApi.Models;
+using System.Collections.Generic;
+using Task = TaskApi.Models.Task;
+
+namespace TaskApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly TaskRepository _taskRepository;
+
+        public TaskController(TaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        // GET: api/Task
+        [HttpGet]
+        public IEnumerable<Task> GetTasks()
+        {
+            return _taskRepository.GetTasks();
+        }
+
+        // GET: api/Task/5
+        [HttpGet("{id}")]
+        public ActionResult<Task> GetTask(int id)
+        {
+            var task = _taskRepository.GetTaskById(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return task;
+        }
+
+        // POST: api/Task
+        [HttpPost]
+        public ActionResult<Task> PostTask(Task task)
+        {
+            _taskRepository.AddTask(task);
+            return CreatedAtAction(nameof(GetTask), new { id = task.TaskID }, task);
+        }
+
+        // PUT: api/Task/5
+        [HttpPut("{id}")]
+        public IActionResult PutTask(int id, Task task)
+        {
+            if (id != task.TaskID)
+            {
+                return BadRequest();
+            }
+
+            _taskRepository.UpdateTask(task);
+            return NoContent();
+        }
+
+        // DELETE: api/Task/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTask(int id)
+        {
+            var task = _taskRepository.GetTaskById(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _taskRepository.DeleteTask(id);
+            return NoContent();
+        }
+    }
+}
---
+++ TaskApi/DataAccess/TaskRepository.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using TaskApi.DataAccess;
+using TaskApi.Models;
+using Task = TaskApi.Models.Task;
+
+namespace TaskApi.DataAccess
+{
+    public class TaskRepository
+    {
+        private readonly string _connectionString;
+
+        public TaskRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddTransient<TaskRepository>();
+        }
+
+        // Create a new Task
+        public void AddTask(Task task)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO Task (TaskName, Description, Duration, AssignedBy) VALUES (@TaskName, @Description, @Duration, @AssignedBy)", connection);
+                command.Parameters.AddWithValue("@TaskName", task.TaskName);
+                command.Parameters.AddWithValue("@Description", task.Description);
+                command.Parameters.AddWithValue("@Duration", task.Duration);
+                command.Parameters.AddWithValue("@AssignedBy", task.AssignedBy);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // Retrieve all Tasks
+        public IEnumerable<Task> GetTasks()
+        {
+            List<Task> tasks = new List<Task>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM Task", connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Task task = new Task
+                    {
+                        TaskID = Convert.ToInt32(reader["TaskID"]),
+                        TaskName = reader["TaskName"].ToString(),
+                        Description = reader["Description"].ToString(),
+                        Duration = Convert.ToInt32(reader["Duration"]),
+                        AssignedBy = reader["AssignedBy"].ToString()
+                    };
+                    tasks.Add(task);
+                }
+            }
+
+            return tasks;
+        }
+
+        // Retrieve a Task by ID
+        public Task GetTaskById(int taskId)
+        {
+            Task task = null;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM Task WHERE TaskID = @TaskID", connection);
+                command.Parameters.AddWithValue("@TaskID", taskId);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    task = new Task
+                    {
+                        TaskID = Convert.ToInt32(reader["TaskID"]),
+                        TaskName = reader["TaskName"].ToString(),
+                        Description = reader["Description"].ToString(),
+                        Duration = Convert.ToInt32(reader["Duration"]),
+                        AssignedBy = reader["AssignedBy"].ToString()
+                    };
+                }
+            }
+
+            return task;
+        }
+
+        // Update an existing Task
+        public void UpdateTask(Task task)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand("UPDATE Task SET TaskName = @TaskName, Description = @Description, Duration = @Duration, AssignedBy = @AssignedBy WHERE TaskID = @TaskID", connection);
+                command.Parameters.AddWithValue("@TaskID", task.TaskID);
+                command.Parameters.AddWithValue("@TaskName", task.TaskName);
+                command.Parameters.AddWithValue("@Description", task.Description);
+                command.Parameters.AddWithValue("@Duration", task.Duration);
+                command.Parameters.AddWithValue("@AssignedBy", task.AssignedBy);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // Delete a Task
+        public void DeleteTask(int taskId)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand("DELETE FROM Task WHERE TaskID = @TaskID", connection);
+                command.Parameters.AddWithValue("@TaskID", taskId);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
